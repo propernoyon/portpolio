e1c56bc5257d0e3b0104f1a12858c4c374d6bf3c
Toriqul Islam <mdtoriqul89@gmail.com>
2019-12-13 15:48:24 +0600

    new

---
+++ js/ripple.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+
+"use strict"
+$('#home-parallex').ripples({
+	resolution:256,
+    dropRadius: 20,
+    perturbance:0.03,
+	interactive:true,
+    
+  });
+
+})
